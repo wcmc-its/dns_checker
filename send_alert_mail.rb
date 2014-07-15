8b52d05318a5f7a8cb1307b438159bc1b515cc21
thomasmmc <thomasmmc@gmail.com>
2014-07-14 23:44:05 -0400

    Changed this to a method that we call in dns_checker

--- send_alert_mail.rb
+++ send_alert_mail.rb
@@ -1,18 +1,21 @@
 require 'net/smtp'
 require 'yaml'
 
+
+def send_message(subject, body)
 #Loading YAML variables
-config = YAML.load_file("dns_checker.config.yml")
+config = YAML.load_file("dns_checker_config.yml")
 recipients = config["recipients"].split(",")
 smtpserver = config["smtpserver"]
 from = config["fromaddress"]
 
-message = <<MESSAGE_END
-From: DNS Checker
-To: #{recipients.join(",")}
-Subject: #{@subject}
 
-#{@body}
+message = <<MESSAGE_END
+From: DNS Checker <#{from}>
+To: #{recipients.join(",")}
+Subject: #{subject}
+
+#{body}
 
 Thank You,
 DNS Checker
@@ -20,4 +23,5 @@ MESSAGE_END
 
 Net::SMTP.start(smtpserver) do |smtp|
   smtp.send_message message, from, recipients
+end
 end
