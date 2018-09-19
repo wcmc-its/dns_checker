74ce4d83668c2318ef036b169b1bb080d0cb1174
Bryan Fisher <brf2010@med.cornell.edu>
2018-09-19 16:26:47 -0400

    added AAAA record support, debug statements

--- dns_checker.rb
+++ dns_checker.rb
@@ -10,17 +10,29 @@ name = config["dnsname"]
 nameserver = config["nameserver"]
 correctip = config["correctip"]
 
+#puts "name #{name}"
+#puts "nameserver #{nameserver}"
+#puts "correctip #{correctip}"
+
+
 #Doing the DNS check
 Resolv::DNS.open({:nameserver=>[nameserver]}) do |r|
-    $resolvedip = r.getaddresses(name)
+#Resolv::DNS.open(resolvers=[,nameserver]) do |r|
+    #$resolvedip = r.getaddresses(name)
+    $A_records = r.getresources name, Resolv::DNS::Resource::IN::A
+    $AAAA_records = r.getresources name, Resolv::DNS::Resource::IN::AAAA
+    $resolvedip = $A_records + $AAAA_records
 end
+
+#puts "resolved IPs #{$resolvedip.inspect}"
  
 #Begining the comparison
 count = $resolvedip.count
 
 resip = Array.new
 $resolvedip.each do |ip|
-	resip << ip.to_s
+	#resip << ip.to_s
+	resip << ip.address.to_s
 end
 
 unknown = Array.new
@@ -43,6 +55,7 @@ if clean.include?('N')
 else
 	send='N'
 end
+
 #going to send that email out if we are setting the send value
 if send == 'Y'
 	send_message(subject, body)#method from send_alert_mail
